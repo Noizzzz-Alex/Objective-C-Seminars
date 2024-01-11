5fcb97d045693fec00c176e2bf387879d2815294
Noizzzz-Alex <Noizzzz@yandex.ru>
2024-01-12 00:21:57 +0700

    Complete Home Work #4

---
+++ Objective-C Seminars/Robot.h
@@ -0,0 +1,16 @@
+//
+//  Robot.h
+//  Objective-C Seminars
+//
+//  Created by Александр Харлампов on 12.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Robot : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C Seminars/Robot.m
@@ -0,0 +1,12 @@
+//
+//  Robot.m
+//  Objective-C Seminars
+//
+//  Created by Александр Харлампов on 12.01.2024.
+//
+
+#import "Robot.h"
+
+@implementation Robot
+
+@end
